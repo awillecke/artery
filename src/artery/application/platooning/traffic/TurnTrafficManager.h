2e36016db77ed1362d6b658c8f97b41797e38eb8
Alexander Willecke <willecke@ibr.cs.tu-bs.de>
2018-08-20 14:29:10 +0200

    Added simple TrafficManager to add a # of one vehilce platoons

---
+++ src/artery/application/platooning/traffic/TurnTrafficManager.h
@@ -0,0 +1,41 @@
+#ifndef TURNTRAFFICMANAGER_H_
+#define TURNTRAFFICMANAGER_H_
+
+#include "artery/traci/TraCIBaseTrafficManager.h"
+
+class TurnTrafficManager : public TraCIBaseTrafficManager
+{
+
+	public:
+
+		virtual void initialize(int stage);
+		virtual void finish();
+
+		TurnTrafficManager() : TraCIBaseTrafficManager() {
+
+		}
+
+	protected:
+		SimTime insertTime;
+		double insertSpeed;
+
+		int nCars;
+		int lane;
+		int nCarsInserted;
+		double insertCarInterval;
+		std::string platooningVType;
+
+		cMessage *insertVehilceMessage;
+
+		//vehicles to be inserted
+		struct Vehicle automated;
+
+		virtual void scenarioLoaded();
+
+		void insertVehicle();
+
+		virtual void handleSelfMsg(cMessage *msg);
+
+};
+
+#endif
